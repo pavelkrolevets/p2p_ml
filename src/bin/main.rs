830ba1a91cc1ba0c62ffc0dded8dacc13492673d
pavelkrolevets <pavelkrolevets@gmail.com>
2020-07-17 11:54:41 +0300

    parse incoming request 	modified:   Cargo.lock 	modified:   Cargo.toml 	modified:   src/bin/main.rs

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -8,10 +8,13 @@ use std::thread;
 use std::time::Duration;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use serde_json::{Deserializer, Value};
+use std::borrow::Borrow;
+use std::borrow::Cow;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    let pool = ThreadPool::new(4);
+    let pool = ThreadPool::new(10);
 
     for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
@@ -25,19 +28,31 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+
+
+
 
     let get = b"GET / HTTP/1.1\r\n";
     let sleep = b"GET /sleep HTTP/1.1\r\n";
+    let post = b"POST / HTTP/1.1\r\n";
+
 
     let (status_line, filename) = if buffer.starts_with(get) {
         ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
     } else if buffer.starts_with(sleep) {
         thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else if  buffer.starts_with(post) {
+        let str = String::from_utf8_lossy(&buffer[..]).to_string();
+        for i in str.split("\r\n") {
+            println!("Request: {}", &i);
+        }
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
     };
+
 
     let contents = fs::read_to_string(filename).unwrap();
     let response = format!("{}{}", status_line, contents);
